1ef5de73854e01be0a6ad3aa30eb3cc0d942cb31
Jennifer Arabel <jennifer.arabel@gmail.com>
2025-07-13 17:48:50 -0300

    Create new structure of files

---
+++ angular-exercises/src/app/components/jugador-add/jugador-add.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jugador-add',
+  imports: [],
+  templateUrl: './jugador-add.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class JugadorAddComponent { }
---
+++ angular-exercises/src/app/components/jugador-list/jugador-list.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jugador-list',
+  imports: [],
+  templateUrl: './jugador-list.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class JugadorListComponent { }
