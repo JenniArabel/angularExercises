bbeaab3d504d25704d2aa3588732719f43d57f3d
Jennifer Arabel <jennifer.arabel@gmail.com>
2025-07-15 08:18:10 -0300

    Add styles to jugadores component

--- angular-exercises/src/app/pages/jugadores/jugadores.component.ts
+++ angular-exercises/src/app/pages/jugadores/jugadores.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-jugadores',
   templateUrl: './jugadores.component.html',
+  styleUrls: ['./jugadores.component.css'],
 })
 export class JugadoresComponent {
   posicionSeleccionada: string = '';
